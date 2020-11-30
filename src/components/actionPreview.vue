047fd094fba5608d2c7c84e10f4b00449888f3e6
theo <theodor.franke@outlook.de>
2020-11-30 22:16:26 +0100

    rearranged code

--- src/components/actionPreview.vue
+++ src/components/actionPreview.vue
@@ -1,19 +1,27 @@
 <template>
     <div class="wrapper" :style="{backgroundColor: settings.backgroundColor['hex']}">
         <div class="opsi-form" :style="formStyle">
-            <canvas :style="backgroundImageStyle"
-                    class="opsi-img"
-                    ref="ImageBgCanvas"
-                    :height="settings.Form.Height - settings.ImageBg.Top"
-                    :width="settings.Form.Width - settings.ImageBg.Left"
-            ></canvas>
+            <background-image-preview
+                    :img-src="settings.ImageBg.imageData"
+                    :height="settings.ImageBg.Height"
+                    :width="settings.ImageBg.Width"
+                    :left="settings.ImageBg.Left"
+                    :top="settings.ImageBg.Top"
+                    :form-height="settings.Form.Height"
+                    :form-width="settings.Form.Width"
+                    :transparent-color="settings.Form.TransparentColor"
+                    :transparent="settings.Form.Transparent"
+            />
         </div>
     </div>
 </template>
 
 <script>
+    import BackgroundImagePreview from "@/components/shared/backgroundImagePreview";
+
     export default {
         name: "actionPreview",
+        components: {BackgroundImagePreview},
         props: {
             settings: Object
         },
@@ -33,49 +41,6 @@
                 else style['backgroundColor'] = this.OpsiRGBtoRGBString(this.settings.Form.Color);
 
                 return style
-            },
-            backgroundImageStyle() {
-                return {
-                    width: this.settings.Form.Width + 'px',
-                    height: this.settings.Form.Height + 'px',
-                    top: this.settings.ImageBg.Top + 'px',
-                    left: this.settings.ImageBg.Left + 'px',
-                }
-            }
-        },
-        methods: {
-            updateCanvas() {
-                let ctx = this.$refs.ImageBgCanvas.getContext('2d');
-                let img = new Image;
-                let height = this.settings.ImageBg.Height;
-                let width = this.settings.ImageBg.Width;
-                let tcolor = this.OpsiRGBtoRGB(this.settings.Form.TransparentColor).rgb;
-                let transparent = this.settings.Form.Transparent;
-                ctx.clearRect(0, 0, this.$refs.ImageBgCanvas.width, this.$refs.ImageBgCanvas.height);
-
-                img.onload = function () {
-                    ctx.drawImage(img, 0, 0, width, height);
-                    if (!transparent) return;
-
-                    let imgData = ctx.getImageData(0, 0, width, height);
-                    let length = imgData.data.length;
-                    for (let i = 0; i < length; i += 4) {
-                        if (imgData.data[i] === tcolor.r && imgData.data[i + 1] === tcolor.g && imgData.data[i + 2] === tcolor.b) {
-                            imgData.data[i + 3] = 0;
-                        }
-                    }
-                    ctx.putImageData(imgData, 0, 0);
-                };
-
-                img.src = this.settings.ImageBg.imageData;
-            }
-        },
-        watch: {
-            settings: {
-                handler() {
-                    this.updateCanvas()
-                },
-                deep: true
             }
         }
     }
@@ -88,7 +53,7 @@
         height: 100%;
     }
 
-    .opsi-form, .opsi-img {
+    .opsi-form {
         position: absolute;
         display: block;
     }
---
+++ src/components/shared/backgroundImagePreview.vue
@@ -0,0 +1,98 @@
+<template>
+    <canvas :style="backgroundImageStyle"
+            class="opsi-img"
+            ref="ImageBgCanvas"
+            :height="formHeight - top"
+            :width="formWidth - left"
+    ></canvas>
+</template>
+
+<script>
+    export default {
+        name: "backgroundImagePreview",
+        props: {
+            imgSrc: String,
+            height: Number,
+            width: Number,
+            left: Number,
+            top: Number,
+            formHeight: Number,
+            formWidth: Number,
+            transparentColor: String,
+            transparent: Boolean
+        },
+        computed: {
+            backgroundImageStyle() {
+                return {
+                    width: this.formWidth + 'px',
+                    height: this.formHeight + 'px',
+                    top: this.top + 'px',
+                    left: this.left + 'px',
+                }
+            }
+        },
+        methods: {
+            updateCanvas() {
+                let ctx = this.$refs.ImageBgCanvas.getContext('2d');
+                let img = new Image;
+                let height = this.height;
+                let width = this.width
+                let tcolor = this.OpsiRGBtoRGB(this.transparentColor).rgb;
+                let transparent = this.transparent;
+                ctx.clearRect(0, 0, this.$refs.ImageBgCanvas.width, this.$refs.ImageBgCanvas.height);
+
+                img.onload = function () {
+                    ctx.drawImage(img, 0, 0, width, height);
+                    if (!transparent) return;
+
+                    let imgData = ctx.getImageData(0, 0, width, height);
+                    let length = imgData.data.length;
+                    for (let i = 0; i < length; i += 4) {
+                        if (imgData.data[i] === tcolor.r && imgData.data[i + 1] === tcolor.g && imgData.data[i + 2] === tcolor.b) {
+                            imgData.data[i + 3] = 0;
+                        }
+                    }
+                    ctx.putImageData(imgData, 0, 0);
+                };
+
+                img.src = this.imgSrc;
+            }
+        },
+        watch: {
+            imgSrc() {
+                this.updateCanvas()
+            },
+            height() {
+                this.updateCanvas()
+            },
+            width() {
+                this.updateCanvas()
+            },
+            left() {
+                this.updateCanvas()
+            },
+            top() {
+                this.updateCanvas()
+            },
+            formHeight() {
+                this.updateCanvas()
+            },
+            formWidth() {
+                this.updateCanvas()
+            },
+            transparentColor() {
+                this.updateCanvas()
+            },
+            transparent() {
+                this.updateCanvas()
+            },
+        }
+    }
+</script>
+
+<style>
+    .opsi-img {
+        position: absolute;
+        display: block;
+    }
+</style>
